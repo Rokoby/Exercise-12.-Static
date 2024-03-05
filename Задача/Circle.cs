9b4078cde331a9a9651f58ad6bbe26a3df18accf
BR <244097@niuitmo.ru>
2024-03-05 16:30:04 +0300

    Добавьте файлы проекта.

---
+++ Задача/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача
+{
+    static class Circle
+    {
+        public static double Length (double r)
+        {
+            return 2*Math.PI*r;
+        }
+        public static double Square (double r)
+        {
+            return Math.PI*r*r;
+        }
+        public static string isLiesOn (double x ,double y, double r, double x0, double y0)
+        {
+            if (Math.Sqrt((x - x0) * (x - x0) + (y - y0) * (y - y0)) == r)
+                return "Точка принадлежит кругу";
+            else 
+                return "Точка не принадлежит кругу";
+        }
+    }
+}
---
+++ Задача/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите r:");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты x0 и y0 центра окружности");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты x и y точки");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Длина окружности: {Circle.Length(r):F2} \n" +
+                $"Площадь круга: {Circle.Square(r):F2} \n" +
+                $"{Circle.isLiesOn(x, y, r, x0, y0)}");
+            Console.ReadKey();
+        }
+    }
+}
